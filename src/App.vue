2c4d726f75b635291c22b411bee29d2e2f8f437e
Khashayar Shomali <khashi792017@gmail.com>
2023-03-05 21:55:15 +0330

    Pass data from child to parent.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
+<!-- This is our Parent Component -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="parent-container">
+    <div class="image">
+      <img src="@/assets/Khashayar-Shomali.jpg" alt="Khashayar Shomali" />
+    </div>
+    <div class="data">
+      <ChildComponent :data="data" />
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { reactive } from "vue";
+import ChildComponent from "./components/ChildComponent.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const data = reactive([
+  {
+    firstName: "Khashayar",
+    lastName: "Shomali",
+    age: 23,
+    gender: "Male",
+    jobTitle: "Front-End Developer",
+  },
+]);
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: sans-serif;
+}
+
+body {
+  background-color: rgb(224, 255, 252);
+}
+
+.image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.image img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  border: 2px solid;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.988), 0 0 20px 0 rgba(0, 0, 0, 0.3);
+}
+
+@media screen and (max-width: 480px) {
+  .image img {
+    width: 150px;
+    height: 150px;
+  }
 }
 </style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="child-container">
+    <h2>Access Parent Data from Child Component | Vue.js 3</h2>
+    <div class="items" v-for="items in data" :key="items">
+      <p>{{ items.firstName }}</p>
+      <p>{{ items.lastName }}</p>
+      <p>{{ items.age }}</p>
+      <p>{{ items.gender }}</p>
+      <p>{{ items.jobTitle }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["data"]);
+</script>
+
+<style>
+.child-container h2 {
+  text-align: center;
+}
+
+.items {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 600;
+  color: rgb(103, 103, 103);
+}
+
+@media screen and (max-width: 480px) {
+  .items {
+    font-size: 14px;
+  }
+}
+
+.child-container h2 {
+  font-size: 22px;
+  padding: 0 40px;
+  line-height: 35px;
+}
+</style>
